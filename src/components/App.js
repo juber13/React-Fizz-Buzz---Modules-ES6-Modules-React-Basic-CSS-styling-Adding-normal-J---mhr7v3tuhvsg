22021854c863ab4b3e3cb1d49adbfadc93eb63f0
Juber <34296950+juber13@users.noreply.github.com>
2023-01-13 19:25:37 +0000

    make buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../styles/App.css';
+// import React , { useState } from 'react';
 const App = () => {
-
+//  const [value , setValue] = useState(1);
   
   return (
     <div id="main">
-
+      <button id="increment">increment</button>
+      <div id="counter">{value}</div>
+      <button id="decrement">decrement</button>
     </div>
   )
 }
