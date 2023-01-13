8a5b5c6a3adc4457ffad8f75ae73d4719f0a8e24
Juber <34296950+juber13@users.noreply.github.com>
2023-01-13 20:24:32 +0000

    make fizz buzz

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
-import React from 'react'
+// import React from 'react'
 import '../styles/App.css';
-// import React , { useState } from 'react';
+import React , { useState } from 'react';
 const App = () => {
-//  const [value , setValue] = useState(1);
+  const [color , setColor] = useState('normal');
+ const [counter , setCounter] = useState(1);
+ 
+ 
   
+ const handleDecrease = () => {
+   if(counter <= 1) return;
+   setCounter(counter - 1);
+   fizzOrBuzz();
+  }
+
+  const handleIncrease = () => {
+    setCounter(counter + 1);
+    fizzOrBuzz();
+  }
+
+  function fizzOrBuzz() {
+    if ((counter % 3 === 0) && (counter % 5 === 0)) setColor('fizzbuzz')
+     else if (counter % 3 === 0) setColor("fizz")
+     else if (counter % 5 === 0) setColor("buzz")
+     else setColor('normal');
+  }
+
+
   return (
     <div id="main">
-      <button id="increment">increment</button>
-      <div id="counter">{value}</div>
-      <button id="decrement">decrement</button>
+      <button id="increment" onClick={handleIncrease}>increment</button>
+      <div id="counter" className={color}>{counter}</div>
+      <button id="decrement" onClick={handleDecrease}>decrement</button>
     </div>
   )
+
 }
 
 
