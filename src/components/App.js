6925cf592bdbb5a0bacf3212712e31db9abab6c5
Juber <34296950+juber13@users.noreply.github.com>
2023-01-17 05:34:43 +0000

    make increase function

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,22 @@
 import '../styles/App.css';
 import React , { useState } from 'react';
 const App = () => {
-  const [color , setColor] = useState('normal');
- const [counter , setCounter] = useState(1);
+  const [counter , setCounter] = useState(1);
+  const [color , setColor] = useState();
  
  
+  function fizzOrBuzz() {
+    if(counter % 3 == 0) setColor("fizz")
+    else if(counter % 5 == 0) setColor("buzz")
+    else if ((counter % 3 == 0) && (counter % 5 == 0)) setColor('fizzbuzz')
+     else setColor('normal');
+  }
+
+  // fizzOrBuzz();
   
  const handleDecrease = () => {
-   if(counter <= 1) return;
-   setCounter(counter - 1);
-   fizzOrBuzz();
+   if(counter > 1) setCounter(counter - 1);
+     fizzOrBuzz();
   }
 
   const handleIncrease = () => {
@@ -18,18 +25,13 @@ const App = () => {
     fizzOrBuzz();
   }
 
-  function fizzOrBuzz() {
-    if ((counter % 3 === 0) && (counter % 5 === 0)) setColor('fizzbuzz')
-     else if (counter % 3 === 0) setColor("fizz")
-     else if (counter % 5 === 0) setColor("buzz")
-     else setColor('normal');
-  }
 
+ 
 
   return (
     <div id="main">
       <button id="increment" onClick={handleIncrease}>increment</button>
-      <div id="counter" className={color}>{counter}</div>
+      <div id="counter" className={color || 'normal'}>{counter}</div>
       <button id="decrement" onClick={handleDecrease}>decrement</button>
     </div>
   )
